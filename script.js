86721393a33fbf6313cf9ac727bb706999f1f81d
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-07-20 22:49:19 +0200

    a

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
- fetch('https://cryptic-mountain-40626-a8a4c6663ade.herokuapp.com/https://games.planetaryannihilation.net/', {
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Other head elements go here -->
+</head>
+<body>
+  <!-- Body elements go here -->
+
+  <script>
+    // JavaScript code goes here
+    fetch('https://cryptic-mountain-40626-a8a4c6663ade.herokuapp.com/https://games.planetaryannihilation.net/', {
         headers: {
             'x-requested-with': 'xhr' 
         }
     })
     .then(response => response.json())
-    .then(data => {
-        // Get the div where we will display the game data
-        const gameDataDiv = document.getElementById('game-data');
-
-        // Iterate over each game in the data
-        data.Games.forEach(game => {
-            // Check if the game is in lobby
-            if(game.GameServerState === "lobby") {
-                // Parse the TitleData into a JSON object
-                const titleData = JSON.parse(game.TitleData);
-
-                // Get the creator, spectators and player count from the TitleData
-                const creator = titleData.creator;
-                const spectators = titleData.spectator_names;
-                const currentPlayers = titleData.players;
-                const maxPlayers = titleData.max_players;
-
-                // Create a new div for this game
-                const gameDiv = document.createElement('div');
-
-                // Create a new text node with the game details
-                const gameText = document.createTextNode(`Game Mode: ${game.GameMode}, 
-                                                         Creator: ${creator}, 
-                                                         Players: ${currentPlayers.length}/${maxPlayers}, 
-                                                         Spectators: ${spectators.join(', ')}`);
-
-                // Add the text to the game div
-                gameDiv.appendChild(gameText);
-
-                // Add the game div to the main game data div
-                gameDataDiv.appendChild(gameDiv);
-            }
-        });
-    })
+    .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
+  </script>
+</body>
+</html>
+
