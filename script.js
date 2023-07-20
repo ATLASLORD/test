abf75fbc9295d5f19764763544ccbc3f075b0d2e
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-07-20 21:44:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:8080/https://games.planetaryannihilation.net/')
+fetch('https://your-app-name.herokuapp.com/https://games.planetaryannihilation.net/')
     .then(response => response.json())
     .then(data => {
         // Get the div where we will display the game data
