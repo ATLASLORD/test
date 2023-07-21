3f1ebd6d0e29e2597318375595d484bb36c76735
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-07-21 12:42:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let cors_api_url = 'https://cryptic-mountain-40626-a8a4c6663ade.herokuapp.com';
-  fetch(cors_api_url, {
-    method: "GET",
-    headers: {
-      "x-requested-with": "xhr" 
-    }
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    document.getElementById("data").innerHTML = JSON.stringify(data, undefined, 2);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
-});
+window.onload = function() {
+    // Variables for the API request
+    var proxyUrl = 'https://cryptic-mountain-40626-a8a4c6663ade.herokuapp.com/';
+    var targetUrl = 'https://games.planetaryannihilation.net/';
+
+    // Make the API request
+    fetch(proxyUrl + targetUrl, { 
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Log the API data
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        // Log any errors
+        console.error('Error:', error);
+    });
+}
